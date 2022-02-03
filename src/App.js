ed7886f5cbae6b219f6011d03ed38151db4deec3
ismail <iduzel49@gmail.com>
2022-02-03 11:42:40 +0100

    services and pricing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import NotFound from './pages/notFound/NotFound';
+import Services from './pages/home/services/Services';
 
 function App() {
   return (
     <div className="App">
      <Routes>
        <Route path="/" element={<Home />} />
+       <Route path="/services" element={<Services />} />
 
        <Route path="*" element={<NotFound />} />
      </Routes>
---
+++ src/pages/home/services/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Services = () => {
+  return <div>
+      <Button> HELLO FROM SERVICES PAGE</Button>
+     </div>;
+};
+
+export default Services;
