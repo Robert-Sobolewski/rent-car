56e79d58e7b3d3b4336fc9a9870488d5424ea23c
Igor <machold.igor@gmail.com>
2022-02-03 12:00:12 +0100

    navbar is fun

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home';
-import NotFound from './pages/notFound/NotFound';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import NotFound from "./pages/notFound/NotFound";
+import Navigation from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-     <Routes>
-       <Route path="/" element={<Home />} />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
 
-       <Route path="*" element={<NotFound />} />
-     </Routes>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../../styles/main.css"
+import { Link } from "react-router-dom";
+
+
+export default function Navigation() {
+
+
+
+  return (
+    <div className="navbar">
+        <h3>Rent Car</h3>
+        <div>
+            <ul>
+                <Link to="/"><li>Home</li></Link>
+                <Link to="/about"><li>About</li></Link>
+                <Link to="/services"><li>Services</li></Link>
+                <Link to="/pricing"><li>Pricing</li></Link>
+                <Link to="/cars"><li>Cars</li></Link>
+                <Link to="/blog"><li>Blog</li></Link>
+                <Link to="/contact"><li>Contact</li></Link>
+         
+            </ul>
+        </div>
+     
+    </div>
+  );
+}
