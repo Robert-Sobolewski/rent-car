b26c63f3ed0f33d4d490b34962f316d7c6818dab
ismail <iduzel49@gmail.com>
2022-02-06 23:30:42 +0100

    footer is made responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,75 @@
-
+import { Col, Container, Row, Stack } from "react-bootstrap";
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import {BiMap} from "react-icons/bi"
+import {AiOutlinePhone} from "react-icons/ai"
+import {FiMail} from "react-icons/fi"
 
 import "./Footer.css";
 
 function Footer() {
- 
   return (
-    <div className="footer pt-5">
+    <Container fluid className="   mt-5">
+      <Row className="bg-dark text-light p-5">
+        <Col className="me-3">
+          <Stack className="">
+            <h2 className="text-danger">CARBOOK</h2>
+            <p>
+              Far far away, behind the word mountains, far from the countries
+              Vokalia and Consonantia, there live the blind texts.
+            </p>
+            <div className="social-media">
+              <span>
+                <FaFacebookF className="sm text-primary fs-3" />
+              </span>
+              <span>
+                <FaTwitter className="sm ms-5 me-5 text-primary fs-3" />
+              </span>
+              <span>
+                <FaInstagram className="sm text-warning fs-3" />
+              </span>
+            </div>
+          </Stack>
+        </Col>
+        <Col>
+          <Stack>
+            <h2 className="text-danger">Information</h2>
+            <Link to="/about">About</Link>
+            <Link to="/services">Services</Link>
+            <Link to="/">Term and Conditions</Link>
+            <Link to="/">Best Price Guarantie</Link>
+            <Link to="/">Privacy & Cookies Policy</Link>
+          </Stack>
+        </Col>
+        <Col>
+          <Stack>
+            <h2 className="text-danger">FAQ</h2>
+            <Link to="/">Payment Option</Link>
+            <Link to="/">Booking Tips</Link>
+            <Link to="/">How it works</Link>
+            <Link to="/contact">Contact Us</Link>
+          </Stack>
+        </Col>
+        <Col>
+          <Stack>
+            <h2 className="text-danger">Have a Question?</h2>
+            <p>
+            <BiMap className="me-2"/>
+              203 Fake St. Mountain View,
+              <br /> San Francisco, <br />
+              California, USA
+            </p>
+            <p>
+              <AiOutlinePhone className="me-2" />
+              +2 392 3929 210</p>
+            <p> 
+              <FiMail className="me-2"/>
+              info@yourdomain.com</p>
+          </Stack>
+        </Col>
+      </Row>
+    </Container>
+    /*  <div className="footer pt-5">
      
 
         <div className="container footer-top">
@@ -71,7 +134,7 @@ function Footer() {
        
  
      
-    </div>
+    </div> */
   );
 }
 
