25cb6f1ac5ca449579e371fda98c46f54d79ab83
Igor <machold.igor@gmail.com>
2022-02-07 15:07:59 +0100

    next commit

--- src/pages/pricing/Pricing.jsx
+++ src/pages/pricing/Pricing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "./Pricing.css";
 import { useSelector } from "react-redux";
