e31d2ff7f8cbc3a17fc6754a952e1e193ff80837
Igor <machold.igor@gmail.com>
2022-02-08 14:57:48 +0100

    done

--- src/components/Home-components/SubmitTrip.js
+++ src/components/Home-components/SubmitTrip.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles/Form styling/form.css"
 import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
 
 export default function SubmitTrip() {
   return (
@@ -8,11 +9,11 @@ export default function SubmitTrip() {
       <h4>Make your trip</h4>
       <div>
         <label htmlFor="text">Pick-up location</label><br />
-        <input type="text" placeholder="City,Airport,Station,etc" id="large-input" />
+        <input type="text" placeholder="City, Airport, Station,etc" id="large-input" />
       </div>
       <div>
         <label htmlFor="">Drop-off location</label><br />
-        <input type="text" placeholder="City,Airport,Station,etc" id="large-input" />
+        <input type="text" placeholder="City, Airport, Station, etc" id="large-input" />
       </div>
 
       <div className="pickup-time">
@@ -31,8 +32,10 @@ export default function SubmitTrip() {
           <label htmlFor="">Pick-up time</label><br />
           <input type="time" id="large-input" />
       </div>
-
+      <Link to="/cars">
       <Button className="mt-3" variant="contained" color="success">Rent A Car now</Button>
+      </Link>
+   
 
     </div>
   );
