2146fd9d3142fa6738dfaf3aebd3d428a3a0f726
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-02-09 13:30:06 +0100

    user id

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -11,7 +11,7 @@ const userSlice = createSlice({
     pick_time: "",
     pick_location: "",
     drop_location: "",
-    car_id: "8",
+    car_id: 0,
     price_h: 0,
     price_d: 82.78,
     price_m: 0,
@@ -47,6 +47,9 @@ const userSlice = createSlice({
     },
     setPassword: (state, action)=>{
         state.password = action.payload;
+    },
+    setId: (state, action)=>{
+        state.car_id = action.payload;
     }
 
 
@@ -66,6 +69,7 @@ export const {
   setEmail,
   setTime,
   getCar,
+  setId,
   setPassword,
 } = userSlice.actions;
 export const selectUser = (state) => state.user.value;
@@ -74,6 +78,6 @@ export const selectDropLoc = (state) => state.user.drop_location;
 export const selectPickDate = (state) => state.user.pick_date;
 export const selectDropDate = (state) => state.user.drop_date;
 export const selectPickTime = (state) => state.user.pick_time;
-export const selectCar = (state) => state.user.car_id;
 export const selectEmail = (state) => state.user.email;
 export const selectPassword = (state) => state.user.password;
+export const selectCarId = (state) => state.user.car_id
