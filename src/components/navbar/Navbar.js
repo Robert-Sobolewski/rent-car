d7f920b0db4f5e0491b0a2c13e4af9202bd1f5a4
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-02-09 11:39:59 +0100

    server config

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,37 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-<<<<<<< HEAD
-import './Navbar.css'
-import Avatar from '@mui/material/Avatar';
-
-=======
 import "./Navbar.css";
->>>>>>> 2b2f53123138cf79d86db7ada288b6023a6c78fe
 
 export default function Navigation() {
   return (
     <div className="navbar sticky-top">
         <div className="nav-container">
 
-<<<<<<< HEAD
-        <h3><span className="text-black">Rent</span><span className="text-success">Car</span> </h3>
-        <div className="menu-bar">
-            <ul>
-              <NavLink activeClassName="active" to="/"><li>Home</li></NavLink>
-                 <NavLink activeClassName="active" to="/about"><li>About</li></NavLink>
-                <NavLink activeClassName="active" to="/services"><li>Services</li></NavLink>
-                <NavLink  activeClassName="active" to="/pricing"><li>Pricing</li></NavLink>
-                <NavLink  activeClassName="active" to="/cars"><li>Cars</li></NavLink>
-                <NavLink  activeClassName="active" to="/contact"><li>Contact</li></NavLink>
-                <NavLink  activeClassName="active" to="/login"><li>Login</li></NavLink>
-                <NavLink  activeClassName="active" to="/login"><li> <Avatar  sx={{ width: 24, height: 24 }}>H</Avatar></li></NavLink>
-         
-               
-         
-            </ul>
-        </div>
-
-=======
           <h3><span className="text-black">Rent</span><span className="text-success">Car</span> </h3>
           <div className="menu-bar">
               <ul>
@@ -44,7 +19,6 @@ export default function Navigation() {
                   <NavLink  activeClassName="active" to="/login"><li>Login</li></NavLink>  
               </ul>
           </div>
->>>>>>> 2b2f53123138cf79d86db7ada288b6023a6c78fe
 
         </div>
       </div>
