8973314f329a906ab228a4fda1b4abe6f0047d15
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-02-09 12:45:04 +0100

    avatar fixed

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
 name:"user",
 initialState:{
-    value:'',
+    value:"",
     email:"",
     password:"",
     pick_date: "",
